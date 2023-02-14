3731824eacc8012b8b5713b6a0df883b6de3f926
NunezAN <alex101nunez@gmail.com>
2023-02-14 11:16:50 -0600

    input is now being updating to correct casher

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Input from "./components/Input";
 
 function App() {
   const [cashiers, setCashiers] = useState([[], [5], [10], [], [2]]);
-  const [newCustomer, setNewCustomer] = useState(0);
+  // const [newCustomer, setNewCustomer] = useState(0);
   return (
     <div className="bg-black h-screen w-full">
-      <Input setNewCustomer={setNewCustomer}></Input>
+      <Input cashiers={cashiers} setCashiers={setCashiers}></Input>
       <div className="flex justify-center gap-6">
-        {cashiers.map((cashier) => (
-          <div className="flex flex-col gap-2">
+        {cashiers.map((cashier, index) => (
+          <div key={index} className="flex flex-col gap-2">
             <div className="h-6 w-6 border-2 border-white"></div>
-            {cashier.map((customer) => (
-              <div className="h-6 w-6 border-2 border-white text-white rounded-full text-sm flex justify-center items-center">
+            {cashier.map((customer, index) => (
+              <div
+                key={index}
+                className="h-6 w-6 border-2 border-white text-white rounded-full text-sm flex justify-center items-center"
+              >
                 {customer}
               </div>
             ))}
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,35 @@
 import React, { useRef } from "react";
 
-const Input = ({ setNewCustomer }) => {
+const Input = ({ cashiers, setCashiers }) => {
   let amountItems = useRef("");
   function handleSubmit(event) {
     event.preventDefault();
     console.log(amountItems.current.value);
-    setNewCustomer(Number(amountItems.current.value));
+    // setNewCustomer(Number(amountItems.current.value));
+    let least_Num = 1e9;
+    let least_Cashier;
+    cashiers.forEach((cashier, id) => {
+      //   console.log(id);
+      const cashierItems = cashier.reduce((sum, value) => sum + value, 0);
+        console.log(cashierItems);
+      if (least_Num > cashierItems) {
+        least_Num = cashierItems;
+        least_Cashier = id;
+        if (least_Num === 0) {
+          return;
+        }
+      }
+    });
+    setCashiers((current) => {
+      //   console.log(current);
+      return current.map((line, id) => {
+        if (least_Cashier === id) {
+          return [...line, Number(amountItems.current.value)];
+        } else {
+          return line;
+        }
+      });
+    });
   }
   return (
     <div className="mx-auto p-4 text-white max-w-xl">
